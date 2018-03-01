5168406bd0e728e5d309a72e9d3165584a9badca
Ubuntu <ubuntu@ip-172-31-18-32.ec2.internal>
2018-03-01 18:40:10 +0000

    buffers test results

--- copy_dub_par.sh
+++ copy_dub_par.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 NUM_PROCESSORS=`awk '/^processor/{n+=1}END{print n}' /proc/cpuinfo`
 PROCESSOR=`cat /proc/cpuinfo | grep "model name" | head -n 1 | sed -r "s/.* (i[0-9\-]+).*/\1/"`
-VERSIONS=('5', '5-2', '5-3')
+VERSIONS=(5 '5-2' '5-3')
 NUM_ITERS=5
 EXP_NUM_PROCESSORS=$NUM_PROCESSORS
 AVG=
 echo "cpuinfo shows ${NUM_PROCESSORS}x${PROCESSOR} processors" 1>&2
-FILE="/home/ubuntu/cpp/results/buffer/${NUM_PROCESSORS}x${PROCESSOR}_`date +%d_%H_%M`.csv"
-echo $FILE
+FILE="/home/ubuntu/cpp/results/buffer/${NUM_PROCESSORS}x8124M_`date +%d_%H_%M`.csv"
 # FILE="./results/${NUM_PROCESSORS}x${PROCESSOR}_`date +%d_%H_%M`.csv"
+echo $FILE
 
 echo "processor, version, num_threads, iteration, time" >> $FILE
 for vers in "${VERSIONS[@]}"; do
-    for proc in `seq 1 ${EXP_NUM_PROCESSORS[@]}`; do
+    for proc in 1 4 8 16 24 36; do
         AVG=0
         for iter in `seq 1 $NUM_ITERS`; do
             echo "Running vers $vers iter $iter with $proc threads" 1>&2
